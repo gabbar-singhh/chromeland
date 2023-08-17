52d821721c7c399a20bad5433219592c64caef73
himanshu <himanshu01.dev@gmail.com>
2023-08-17 14:22:44 +0530

    clean-up! :done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Chromeland</title>
+        <meta name="description" content="a productivity focused homepage!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
