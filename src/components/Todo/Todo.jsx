d21ec6f2489573ec6aec83664f98a3a111eb1c5f
gabbar <himanshu01.dev@gmail.com>
2023-11-06 23:44:39 +0530

    note&todos are done

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,13 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import styles from "./Todo.module.css";
-import Draggable from "react-draggable";
-import supabase from "@/lib/supabaseClient";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import TodosDataContext from "../ContextAPI/TodosDataContext";
-import { da } from "date-fns/locale";
-import { isValid } from "@/utility/isValid";
-import { isEmpty } from "lodash";
-import Spinner from "../Extras/Spinner/Spinner";
 
 const Todo = () => {
   const todoContext = useContext(TodosDataContext);
@@ -16,32 +10,18 @@ const Todo = () => {
 
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-  const [syncMessage, setSyncMessage] = useState("sync todos");
 
   // WHEN PAGE OPEN, IT CHECKS FOR PREVIOUS LOCAL-STORAGE
-  // useEffect(() => {
-  //   const data = JSON.parse(localStorage.getItem("localTodo"));
-  //   console.log(data);
+  useEffect(() => {
+    if (!isLoading && user) {
+      const data = JSON.parse(localStorage.getItem(`localTodo_${user.email}`));
+      console.log(data);
 
-  //   if (data !== null) {
-  //     setTodos(data);
-  //   }
-  // }, []);
-
-  // 🔴🔴 SYNC MESSAGE TEXT INDICATOR
-  async function fetchExistingTodos(email) {
-    const { data, error } = await supabase
-      .from("todos")
-      .select("todos")
-      .eq("email_id", email);
-
-    if (error) {
-      console.error("Error fetching user data:", error.message);
-      return null;
+      if (data !== null) {
+        setTodos(data);
+      }
     }
-
-    return data[0].todos || {};
-  }
+  }, []);
 
   // WHEN ENTER KEY IS PRESSED
   const keyDownHandler = (event) => {
@@ -50,7 +30,10 @@ const Todo = () => {
         console.log("-->", todo);
         const newTodo = { id: new Date().getTime().toString(), title: todo };
         setTodos([...todos, newTodo]);
-        localStorage.setItem("localTodo", JSON.stringify([...todos, newTodo]));
+        localStorage.setItem(
+          `localTodo_${user.email}`,
+          JSON.stringify([...todos, newTodo])
+        );
         setTodo("");
       }
     }
@@ -60,77 +43,10 @@ const Todo = () => {
   const deleteItemHandler = (todo) => {
     const deleted = todos.filter((t) => t.id !== todo);
     setTodos(deleted);
-    localStorage.setItem("localTodo", JSON.stringify(deleted));
-  };
-
-  const insertEmptyJsonToTodosTable = async (email) => {
-    const insertData = await supabase
-      .from("todos")
-      .insert([
-        {
-          todos: [],
-          email_id: email,
-        },
-      ])
-      .select();
-  };
-
-  async function fetchLatestTodos() {
-    const { data, error } = await supabase
-      .from("todos")
-      .select("todos")
-      .eq("email_id", user.email);
-
-    if (error) {
-      console.error("Error fetching user data:", error.message);
-      return null;
-    }
-
-    return data[0].todos || {};
-  }
-
-  const sendTodos = async (updatedData) => {
-    const { data, error } = await supabase
-      .from("todos")
-      .update({ todos: updatedData })
-      .eq("email_id", user.email)
-      .select();
-
-    if (error) {
-      console.error("Error updating user data:", error.message);
-      return false;
-    }
-
-    return data[0].todos | {};
-  };
-
-  const syncDataHandler = async () => {
-    if (todos.length === 0) {
-      setSyncMessage("syncing todos");
-      const existingTodos = await fetchExistingTodos(user.email);
-      setTodos(existingTodos);
-      setSyncMessage("sync todos");
-    }
-
-    if (todos.length > 0) {
-      setSyncMessage("syncing todos");
-      console.log("dman kaul!", user.email, todos);
-
-      const { error } = await supabase
-        .from("todos")
-        .delete()
-        .eq("email_id", user.email);
-
-      insertEmptyJsonToTodosTable(user.email);
-
-      const updated = await sendTodos(todos);
-
-      setSyncMessage("sync todos");
-    }
+    localStorage.setItem(`localTodo_${user.email}`, JSON.stringify(deleted));
   };
 
   return (
-    // <Draggable>
     <main className={styles.container_todo}>
       <div className={styles.input_box}>
         <input
@@ -141,16 +57,8 @@ const Todo = () => {
           placeholder="Add a task here..."
         />
 
-        <span
-          className={styles.sync_box}
-          onClick={() => console.log("you clicked sync box!")}
-        >
-          <img src="/icons/sync-icon.svg" height={10} alt="" />
-          <p onClick={syncDataHandler}>{syncMessage}</p>
-        </span>
-
         <p>
-          You've currently{" "}
+          you've{" "}
           {!todos.length
             ? "0 task"
             : todos.length === 1
@@ -178,7 +86,6 @@ const Todo = () => {
         })}
       </div>
     </main>
-    // </Draggable>
   );
 };
 
