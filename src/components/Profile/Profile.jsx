d58ed6c35764e722e2e3e884a01b55f61aceaaf5
gabbar <himanshu01.dev@gmail.com>
2023-11-07 03:16:42 +0530

    profile dropdown menu 👎

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Profile.module.css";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import Link from "next/link";
 
 const Profile = ({ name, profile_url, signOut }) => {
     const { user, error, isLoading } = useUser();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeDropdown = () => {
+        setIsOpen(false);
+    };
 
     useEffect(() => {
         console.log("user", user);
@@ -14,16 +24,34 @@ const Profile = ({ name, profile_url, signOut }) => {
         <section className={styles.main}>
             {/* <section className={styles.main} onClick={signOut}> */}
 
-            <div className={styles.profile_box}>
-                <img className={styles.profile_img} src={profile_url} alt="profile image" />
+            <div onClick={toggleDropdown} className={styles.profile_box}>
+                <img
+                    className={styles.profile_img}
+                    src={profile_url}
+                    alt="profile image"
+                />
 
                 <span className={styles.profile_text}>
                     <p>{name}</p>
-                    <p>{'logged in'}</p>
+                    <p>{"logged in"}</p>
                 </span>
-
             </div>
 
+            <div className={styles.dropdown}>
+                {isOpen && (
+                    <div className={styles.dropdown_content}>
+                        <Link onClick={closeDropdown} className={styles.dropdown_item} href="#">
+                            <p>Profile</p>
+                        </Link>
+                        <Link onClick={closeDropdown} className={styles.dropdown_item} href="#">
+                            <p>feedback:)</p>
+                        </Link>
+                        <Link onClick={closeDropdown} className={styles.dropdown_item} href="#">
+                            <p style={{ color: "red" }}>log out</p>
+                        </Link>
+                    </div>
+                )}
+            </div>
         </section>
     );
 };
